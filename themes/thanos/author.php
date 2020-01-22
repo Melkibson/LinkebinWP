f526c8585c0e7bd675a0d11cbc9319e4ac855484
Yamna Melki <melkiyamna@protonmail.com>
2020-01-22 16:23:07 +0100

    User profile front-end finished

--- themes/thanos/author.php
+++ themes/thanos/author.php
@@ -1,6 +1,11 @@
 <?php
 $user_id = get_current_user_id();
 $username = get_the_author_meta( 'user_login', $user_id);
+$email = get_the_author_meta('user_email', $user_id);
+$password = get_the_author_meta('user_pass', $user_id);
+$firstname = get_the_author_meta('user_firstname', $user_id);
+$lastname = get_the_author_meta('user_lastname', $user_id);
+$address = get_the_author_meta('user_description', $user_id);
 ?>
 
 <!doctype html>
@@ -14,41 +19,34 @@ $username = get_the_author_meta( 'user_login', $user_id);
 </head>
     <nav class="form-nav d-flex justify-content-between">
         <img class="h-100" src="<?= get_template_directory_uri() . '/assets/img/logo_linkebin.png'?>" width="150px" alt="">
-        <a href="<?= home_url('/') ?>">Retour</a>
+        <a href="<?= home_url('/') ?>"><img src="<?= get_template_directory_uri() . '/assets/img/back.svg'?>" width="40px" alt=""></a>
     </nav>
 
-    <body id="user-page" class="container-fluid">
-    <?php
-
-//        $url = 'http://localhost:8000/AddUser/' . $user_id;
-//        $response = wp_remote_get($url);
-//        $body = wp_remote_retrieve_body($response);
-//        var_dump($body);
-        ?>
-        <div class="row h-100">
-            <div class="col-12">
-                <h2 class="profile-title text-left">Bonjour <span class="text-capitalize"><?php echo $username; ?></span></h2>
+    <body id="user-page" class="container-fluid h-100">
+        <div class="row px-3">
+            <div id="title" class="col-12">
+                <h2 class="profile-title text-left">Bonjour <span class="text-capitalize"><?= $username; ?></span></h2>
             </div>
             <div class="col-7">
                 <form method="post" class=" form-login shadow-lg p-0">
-                    <div class="col-10 m-auto form-content">
+                    <div class="col-11 m-auto form-content">
                         <div class="row m-auto">
                             <div class="form-group col-6">
                                 <label for="login">Identifiant</label>
-                                <input type="text" class="form-control" id="login" name="login">
+                                <input type="text" class="form-control" id="login" name="login" value="<?= $username?>">
                             </div>
                             <div class="form-group col-6">
                                 <label for="email">Email</label>
-                                <input type="email" class="form-control" id="email" name="email">
+                                <input type="email" class="form-control" id="email" name="email" value="<?= $email ?>">
                                 <span><?php if (isset($errors['user_email'])) : echo $errors['user_email']; endif;?></span>
                             </div>
                         </div>
-                        <div>
-                        <div class="form-group d-flex">
+                        <div class="pt-3">
+                        <div class="reset-pwd form-group d-flex justify-content-between">
                             <label for="password">Ancien mot de passe</label>
                             <input type="password" class="form-control" id="old-password" name="old-password" placeholder="•••••••••">
                         </div>
-                        <div class="form-group">
+                        <div class="reset-pwd form-group d-flex justify-content-between">
                             <label for="password">Nouveau mot de passe</label>
                             <input type="password" class="form-control" id="password" name="password" placeholder="•••••••••">
                         </div>
@@ -59,7 +57,7 @@ $username = get_the_author_meta( 'user_login', $user_id);
                             <span id="number" class="invalid">Un chiffre</span><br>
                             <span id="length" class="invalid">8 caracteres</span>
                         </div>
-                        <div class="form-group">
+                        <div class="reset-pwd form-group d-flex justify-content-between">
                             <label for="password2">Confirmer le mot de passe</label>
                             <input type="password" class="form-control" id="password2" name="password2" placeholder="•••••••••">
                             <div id="message-pwd">
@@ -67,28 +65,37 @@ $username = get_the_author_meta( 'user_login', $user_id);
                             </div>
                         </div>
                         <div class="form-group pt-3">
-                            <input type="submit" class="shadow" id="submit" name="submitted" value="envoyer">
+                            <input type="submit" class="shadow" id="submit" name="submitted" value="sauvegarder">
                         </div>
                     </div>
                 </form>
             </div>
-                <div class="col-5">
-                    <form method="post" class=" form-login shadow-lg p-0">
-                        <div class="col-10 m-auto form-content">
-                            <div class="form-group">
-                                <label for="lastname">Nom</label>
-                                <input type="text" class="form-control" id="lastname" name="lastname">
-                                <label for="firstname">Prenom</label>
-                                <input type="text" class="form-control" id="firstname" name="firstname">
-                            </div>
-                            <div class="form-group">
-                                <label for="address">Adresse</label>
-                                <input type="text" class="form-control" id="address" name="address">
-                            </div>
-                        </div>
-                    </form>
-                </div>
         </div>
-
+        <div class="col-5">
+            <form method="post" class=" form-login shadow-lg p-0">
+                <div class="col-11 m-auto form-content">
+                    <div class="row m-auto">
+                    <div class="form-group col-6">
+                        <label for="lastname">Nom</label>
+                        <input type="text" class="form-control" id="lastname" name="lastname"
+                        <?php if (isset($lastname)):?> value="<?= $lastname; endif;?>">
+                    </div>
+                    <div class="form-group col-6">
+                        <label for="firstname">Prenom</label>
+                        <input type="text" class="form-control" id="firstname" name="firstname"
+	                        <?php if (isset($firstname)):?> value="<?= $firstname; endif;?>">
+                    </div>
+                    <div class="form-group col-12">
+                        <label for="address">Adresse</label>
+                        <input type="text" class="form-control" id="address" name="address"
+	                        <?php if (isset($address)):?> value="<?= $address; endif;?>">
+                    </div>
+                        <div class="form-group pt-3">
+                            <input type="submit" class="shadow" id="submit" name="submitted" value="sauvegarder">
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
 <?php
 get_footer();
